b7fcaf054c6286b7cbb7bbe5c05ad63e5b4a612d
rlaalstjr2465 <rlaalstjr2465@gmail.com>
2024-05-14 11:24:24 +0900

    for문 1~10

---
+++ mission/src/mission/Mission.java
@@ -0,0 +1,13 @@
+package mission;
+
+import java.util.Iterator;
+
+public class Mission {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 11; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ mission/src/module-info.java
@@ -0,0 +1,2 @@
+module mission {
+}
